ea689b15925dd8ee3f340cfa8a9c9a4470251108
Hyung <13234540727@163.com>
2019-10-20 11:51:06 +0800

    v1.1

--- index.js
+++ index.js
@@ -2,20 +2,12 @@ $(function () {
 
   var initHeight = 200, // 初始高度
     usedWidth = 0, // 当前行已占用宽度
-    winWidth = $(window).width(), // 可视区的宽
+    winWidth = $(window).width() - 20, // 可视区的宽(减去滚动条的宽)
+    num = 0, // 当前行图片数量
     img = []; // 图片
 
   var wrapper = $(".wrapper");
 
-
-  // 获取图片
-  function getImg() {
-    var info = {}; // 图片信息（kwidth, height, url）
-    info.width = parseInt(Math.random() * 400 + 100); // 生成 100 - 500 之间随机数
-    info.height = parseInt(Math.random() * 400 + 100); // 生成 100 - 500 之间随机数
-    info.url = "http://placekitten.com/" + info.width + "/" + info.height;
-    return info;
-  }
 
   // 初始化
   (function () {
@@ -32,32 +24,51 @@ $(function () {
     }
   })
 
-  // 添加图片
+  // 窗口可视区宽度改变事件
+  $(window).resize(function () {
+    // 当可视区宽度改变时重新排列图片，并回到顶部
+    if (winWidth !== $(window).width()) {
+      winWidth = $(window).width(); // 重新获取可视区的宽
+      usedWidth = 0; // 重新设置当前行已占用宽度
+      num = 0; // 重新设置当前行图片数量
+      for (var i = 0; i < img.length; i++) {
+        setPic(i);
+      }
+      $(document).scrollTop(0);
+    }
+  })
+
+  // 添加新图片
   function addPic() {
-    var tr = img.length; // 行数
-    var td = tr ? img[tr - 1].length : 0; //当前行图片数
-    var info = getImg(); //图片信息
-    var canUseWidth = winWidth - 40 - td * 4; //当前行可用宽度（可视区宽度 - margin）
-    image = $("<img src='" + info.url + "'>"); // 创建图片
-    var width = info.width * initHeight / info.height; // 当height为initHeight时，当前图片将占用的宽度
+    var len = img.length;
+    var width = parseInt(Math.random() * 400 + 100); // 图片的宽，100 - 500 之间随机数0 - 500 之间随机数
+    var height = parseInt(Math.random() * 400 + 100); // 图片的高，100 - 500 之间随机数00 - 500 之间随机数
+    var url = "http://placekitten.com/" + width + "/" + height; // 图片的url
+    img[len] = $("<img src='" + url + "'>"); // 创建图片
+    img[len].w = width; // 当前图片的宽
+    img[len].h = height; // 当前图片的高
+    wrapper.append(img[len]); // 插入图片
+    setPic(len);
+  }
+
+  // 排列图片
+  function setPic(index) {
+    var canUseWidth = winWidth - 17 - num * 4; //当前行可用宽度（可视区宽度 - margin）
+    var width = img[index].w * initHeight / img[index].h; // 当height为initHeight时，当前图片将占用的宽度
     var lastWidth = canUseWidth - usedWidth; // 当前行剩余宽度
     // 如果该图片不是第一张图片并且当前行有足够宽度放下这张图片
-    if (usedWidth && width < lastWidth) {
+    if (num && width < lastWidth) {
+      num++;
       usedWidth += width; // 更新已占有宽度
-      img[tr - 1][td] = image; // 把当前图片存到img中
-      img[tr - 1][td].w = info.width; // 当前图片的宽
-      img[tr - 1][td].h = info.height; // 当前图片的高
     } else {
       var height = initHeight * canUseWidth / usedWidth; // 重新设置当前行高度
       // 遍历当前行
-      $.each(img[tr - 1], function (index, item) {
-        item.height(height); // 设置图片高
-        item.width(item.w * height / item.h); // 设置图片宽
-        wrapper.append(item); // 插入图片
-      })
+      for (var i = index - num; i < index; i++) {
+        img[i].height(height); // 设置图片高
+        img[i].width(img[i].w * height / img[i].h); // 设置图片宽
+      }
+      num = 1;
       usedWidth = width; // 更新已用宽
-      img[tr] = [];
-      img[tr][0] = image; // 把当前图片存到img中
     }
   }
 
